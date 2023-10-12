95085d66cbaa57670496f257eb2014ca86029fa0
Denis <lusuidenis@gmail.com>
2023-10-12 19:25:58 +0300

    test

--- pins_1_room.py
+++ pins_1_room.py
@@ -428,6 +428,25 @@ class CheckPinTask(threading.Thread):
             self.execute()
 
 
+class CheckCardTask(threading.Thread):
+
+    def __init__(self, interval, execute):
+        threading.Thread.__init__(self)
+        self.daemon = False
+        self.stopped = threading.Event()
+        self.interval = interval
+        self.execute = execute
+
+    def stop(self):
+        self.stopped.set()
+        self.join()
+
+    def run(self):
+        while not self.stopped.wait(self.interval.total_seconds()):
+            self.execute()
+
+
+
 class CheckActiveCardsTask(threading.Thread):
     def __init__(self, interval, execute, *args, **kwargs):
         threading.Thread.__init__(self)
@@ -521,7 +540,7 @@ def main():
     check_pin_task.start()
 
     cardreader_find()
-    cardreader_task = CheckPinTask(interval=timedelta(seconds=system_config.check_pin_timeout), execute=cardreader_find)
+    cardreader_task = CheckCardTask(interval=timedelta(seconds=2), execute=cardreader_find)
     cardreader_task.start()
 
 

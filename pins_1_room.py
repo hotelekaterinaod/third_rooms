cd9daeacca1a06f7fda45370d30ed6975e621a03
Denis <lusuidenis@gmail.com>
2024-09-06 14:53:20 +0300

    Add run py

--- pins_1_room.py
+++ pins_1_room.py
@@ -284,7 +284,7 @@ def init_room():
     logger.info("Init room")
     pin_structure = {
         0: None,
-        1: None
+        1: None,
         # кнопка-выключатель бра правый спальня1,
         2: None,
         3: None,

60d1a76ffc303952e58ee52d58f201567a8ea743
Denis <lusuidenis@gmail.com>
2024-09-06 15:13:42 +0300

    Add run py

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
-import RPi.GPIO as GPIO
+import pigpio
 
-GPIO.setmode(GPIO.BOARD)
+pi = pigpio.pi()
 
-pins_to_test = [7, 11, 13, 15]  # Замените на нужные вам пины
+pins_to_test = [4, 17, 27, 22]  # Замена номеров GPIO на ваши
 
 for pin in pins_to_test:
     try:
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(pin, GPIO.RISING)
-        print(f"Pin {pin} is available for event detection")
-    except RuntimeError as e:
-        print(f"Pin {pin} is not available: {e}")
+        pi.set_mode(pin, pigpio.INPUT)
+        pi.set_pull_up_down(pin, pigpio.PUD_DOWN)
+        pi.callback(pin, pigpio.RISING_EDGE, lambda x, y, z: print(f"Pin {pin} triggered"))
+        print(f"Pin {pin} is set up for edge detection")
+    except Exception as e:
+        print(f"Pin {pin} setup failed: {e}")
 
-GPIO.cleanup()
+pi.stop()

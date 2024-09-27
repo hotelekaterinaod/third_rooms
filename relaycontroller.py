d2b33ce5beb3a690fdfb827ee3794cfc8491838b
Denis <lusuidenis@gmail.com>
2024-09-27 15:56:29 +0300

    New Version

--- relaycontroller.py
+++ relaycontroller.py
@@ -1,31 +1,71 @@
 import smbus
-from config import logger
-
+import time
 
 class RelayController:
-
-    __address = None
-    __bus = None
-    __state = 0xff
-
-    def __init__(self, address):
+    def __init__(self, address, bus_num=1):
+        """
+        Конструктор инициализирует I2C-шину и адрес устройства.
+        """
         self.__address = address
-        self.__bus = smbus.SMBus(1)
-        self.__bus.write_byte_data(self.__address, 0x09, self.__state)
+        self.__bus = smbus.SMBus(bus_num)
+        self.__state = '11111111'  # Начальное состояние (все биты установлены в 1)
+        self.__bus.write_byte_data(self.__address, 0x09, int(self.__state, 2))
+        print(f"Initial state for address {self.__address}: {bin(int(self.__state, 2))}")
 
-    def clear_bit(self, bit):
-        self.__state &= ~(1 << bit)
-        self.__bus.write_byte_data(self.__address, 0x09, self.__state)
+    def set_state(self, state, delay=0.2):
+        """
+        Устанавливает полное состояние для всех битов сразу.
+        """
+        self.__state = f'{state:08b}'  # Преобразуем в двоичное строковое представление
+        print(f"Set full state {bin(int(self.__state, 2))} for address {self.__address}")
+        self.__bus.write_byte_data(self.__address, 0x09, int(self.__state, 2))
+        time.sleep(delay)
 
-    def set_bit(self, bit):
-        logger.info(f"Before for {self.__address} - state  {self.__state}")
-        self.__state |= 1 << bit
-        logger.info(f"Set after state    {self.__state}")
-        self.__bus.write_byte_data(self.__address, 0x09, self.__state)
+    def set_bit(self, bit, delay=0.2):
+        """
+        Устанавливает конкретный бит в 1, обновляя состояние.
+        """
+        print(f"Before set bit {bit} for address {self.__address}: {self.__state}")
+        state_list = list(self.__state)
+        state_list[bit] = '1'  # Меняем бит на 1 (нумерация слева направо)
+        self.__state = ''.join(state_list)
+        print(f"Set bit {bit}, new state: {bin(int(self.__state, 2))}")
+        self.__bus.write_byte_data(self.__address, 0x09, int(self.__state, 2))
+        time.sleep(delay)
 
-    def toggle_bit(self, bit):
-        self.__state ^= 1 << bit
-        self.__bus.write_byte_data(self.__address, 0x09, self.__state)
+    def clear_bit(self, bit, delay=0.2):
+        """
+        Сбрасывает конкретный бит в 0, обновляя состояние.
+        """
+        print(f"Before clear bit {bit} for address {self.__address}: {self.__state}")
+        state_list = list(self.__state)
+        state_list[bit] = '0'  # Меняем бит на 0
+        self.__state = ''.join(state_list)
+        print(f"Clear bit {bit}, new state: {bin(int(self.__state, 2))}")
+        self.__bus.write_byte_data(self.__address, 0x09, int(self.__state, 2))
+        time.sleep(delay)
+
+    def toggle_bit(self, bit, delay=0.2):
+        """
+        Переключает бит (вкл/выкл), обновляя состояние.
+        """
+        state_list = list(self.__state)
+        state_list[bit] = '0' if state_list[bit] == '1' else '1'  # Инвертируем бит
+        self.__state = ''.join(state_list)
+        print(f"Toggle bit {bit}, new state: {bin(int(self.__state, 2))}")
+        self.__bus.write_byte_data(self.__address, 0x09, int(self.__state, 2))
+        time.sleep(delay)
 
     def check_bit(self, bit):
-        return (self.__state >> bit) & 1
+        """
+        Проверяет состояние конкретного бита (0 или 1).
+        """
+        bit_state = self.__state[bit]
+        print(f"Check bit {bit} for address {self.__address}: {bit_state}")
+        return bit_state
+
+    def get_state(self):
+        """
+        Возвращает текущее состояние всех битов в виде целого числа.
+        """
+        return int(self.__state, 2)  # Преобразуем строку в число

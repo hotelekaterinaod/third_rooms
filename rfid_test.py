a2a49e496bbf0185399aaaac91446e644190967a
Denis <lusuidenis@gmail.com>
2024-09-27 19:32:35 +0300

    New Version

--- rfid_test.py
+++ rfid_test.py
@@ -1,5 +1,6 @@
 import serial
 from config import system_config, logger
+from datetime import datetime
 
 def wait_for_rfid(port='/dev/ttyS0', baudrate=9600, timeout=1):
     try:
@@ -9,12 +10,16 @@ def wait_for_rfid(port='/dev/ttyS0', baudrate=9600, timeout=1):
             print(f"Listening for RFID on {port}")
             read_byte = (rfid_port.read(system_config.rfid_key_length)[1:11])
             key_ = read_byte.decode("utf-8")
+            read_byte2 = rfid_port.read(system_config.rfid_key_length)
+            key_2 = read_byte2.decode("utf-8")
             rfid_port.close()
-            if key_:
+            if key_ or key_2:
                 logger.info("key catched {key} {datetime}".format(key=key_, datetime=datetime.utcnow()))
                 return key_
             else:
                 logger.info(f"No key {key_}")
+                read_byte = rfid_port.read(system_config.rfid_key_length)
+                print(f"Raw data: {read_byte}")
     except serial.SerialException as e:
         print(f"Serial error: {e}")
     except Exception as e:

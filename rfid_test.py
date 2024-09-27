db6fe056f8e0dd9e0bcbbcd5f3cf9c0b1b1ff436
Denis <lusuidenis@gmail.com>
2024-09-27 19:33:10 +0300

    New Version

--- rfid_test.py
+++ rfid_test.py
@@ -12,7 +12,7 @@ def wait_for_rfid(port='/dev/ttyS0', baudrate=9600, timeout=1):
             key_ = read_byte.decode("utf-8")
             read_byte2 = rfid_port.read(system_config.rfid_key_length)
             key_2 = read_byte2.decode("utf-8")
-            rfid_port.close()
+
             if key_ or key_2:
                 logger.info("key catched {key} {datetime}".format(key=key_, datetime=datetime.utcnow()))
                 return key_
@@ -20,6 +20,7 @@ def wait_for_rfid(port='/dev/ttyS0', baudrate=9600, timeout=1):
                 logger.info(f"No key {key_}")
                 read_byte = rfid_port.read(system_config.rfid_key_length)
                 print(f"Raw data: {read_byte}")
+            rfid_port.close()
     except serial.SerialException as e:
         print(f"Serial error: {e}")
     except Exception as e:
